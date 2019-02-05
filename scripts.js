bd8b30562928ba1cec9151d8ca685af51e6c1eba
CAN BERK ÖCALIR <can.ocalir@gmail.com>
2019-02-05 19:36:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,28 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-    if(input.value.length > 0) {
+function inputLength () {
+    return input.nodeValue.length;
+}
+
+function createListElement() {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value))
+    li.appendChild(document.createTextNode(input.nodeValue));
     ul.appendChild(li);
-    input.value = "";
+    input.nodeValue = "";
+}
+
+function addListAfterClick () {
+    if (inputLength() > 0) {
+        createListElement();
     }
-})
+}
+
+function addListAfterKeypress (event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
